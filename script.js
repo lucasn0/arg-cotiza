7fcc145a09248281dd28ff851af047b0ddc463a3
Lucas <altingdesigns88@gmail.com>
2022-12-27 03:48:33 -0300

    api

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const cotizaciones = "https://www.dolarsi.com/api/api.php?type=valoresprincipales"
+let test = []
 
 async function fetchDollar() {
     const response = await fetch(cotizaciones);
@@ -7,9 +8,15 @@ async function fetchDollar() {
 }
 
 fetchDollar().then(values => {
-    const oficialCompra = document.getElementById('soyGay');
-    const oficialVenta = document.getElementById
-    console.log(values[0].casa.compra);
-    oficialCompra.innerHTML = values[0].casa.venta;
-    
+    const mayorista = document.getElementById('mayorista');
+    const blue = document.getElementById('blue');
+    const turista = document.getElementById('turista');
+    const ccl = document.getElementById('ccl');
+    const bolsa = document.getElementById('bolsa');
+
+    mayorista.appendChild(document.createElement('p')).innerHTML = `&nbsp; Compra: ${values[0].casa.compra}. Venta: ${values[0].casa.venta}.`;
+    blue.appendChild(document.createElement('p')).innerHTML = `&nbsp; Compra: ${values[1].casa.compra}. Venta: ${values[1].casa.venta}.`;
+    turista.appendChild(document.createElement('p')).innerHTML = `&nbsp; Venta: ${values[6].casa.venta}.`;
+    ccl.appendChild(document.createElement('p')).innerHTML = `&nbsp; Compra: ${values[3].casa.compra}. Venta: ${values[3].casa.venta}.`;
+    bolsa.appendChild(document.createElement('p')).innerHTML = `&nbsp; Compra: ${values[4].casa.compra}. Venta: ${values[4].casa.venta}.`;
 });
